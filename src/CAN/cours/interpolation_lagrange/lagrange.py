9ef72ea1427fdcca890b6fd8390e1cc7f3b62208
flotare <laporte.florian15@gmail.com>
2025-07-20 15:45:24 +0200

    lagrange to merge

--- src/CAN/cours/interpolation_lagrange/lagrange.py
+++ src/CAN/cours/interpolation_lagrange/lagrange.py
@@ -8,7 +8,20 @@ class Lagrange:
     def interpolation_lagrange_locale(
         self, degre: int, list_x: list[int | float], list_y: list[int | float]
     ) -> Callable[[int | float], float]:
-        pass
+        def P(x: int | float) -> float:
+            k = 0
+            n = len(list_x)
+            while k + degre < n and x > list_x[k + degre]:
+                k += degre
+            if k + degre >= n:
+                k = n - degre - 1
+
+            list_x_k = list_x[k: k + degre + 1]
+            list_y_k = list_y[k: k + degre + 1]
+
+            P_local = self.interpolation_lagrange_globale(list_x=list_x_k, list_y=list_y_k)
+            return P_local(x)
+        return P
 
     def interpolation_lagrange_globale(
         self, list_x: list[int | float], list_y: list[int | float]
