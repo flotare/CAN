c98a78ee566e9a67368850b8e75751eb75549860
flotare <laporte.florian15@gmail.com>
2025-07-20 03:45:28 +0200

    Lagrange global

---
+++ src/CAN/cours/interpolation_lagrange/lagrange.py
@@ -0,0 +1,41 @@
+from typing import Callable
+
+
+class Lagrange:
+    def __init__(self):
+        self.list_polynomes_lagrange = []
+
+    def interpolation_lagrange_locale(
+        self, degre: int, list_x: list[int | float], list_y: list[int | float]
+    ) -> Callable[[int | float], float]:
+        pass
+
+    def interpolation_lagrange_globale(
+        self, list_x: list[int | float], list_y: list[int | float]
+    ) -> Callable[[int | float], float]:
+        self.set_list_polynomes_lagrange(list_x=list_x)
+
+        def P(x: int | float) -> float:
+            p_interpole = 0
+            for index in range(len(list_x)):
+                p_interpole += list_y[index] * self.list_polynomes_lagrange[index](x)
+            return p_interpole
+
+        return P
+
+    def set_list_polynomes_lagrange(self, list_x: list[int | float]) -> None:
+        self.list_polynomes_lagrange.clear()
+        for k, x_k in enumerate(list_x):
+            x_copie = [x_i for i, x_i in enumerate(list_x) if i != k]
+            self.set_polynome_lagrange(x_k=x_k, list_x=x_copie)
+
+    def set_polynome_lagrange(
+        self, x_k: int | float, list_x: list[int | float]
+    ) -> None:
+        def f(x: int | float) -> float:
+            produit = 1
+            for x_i in list_x:
+                produit *= (x - x_i) / (x_k - x_i)
+            return produit
+
+        self.list_polynomes_lagrange.append(f)
--- src/CAN/partiel/sujet2024_2025.py
+++ src/CAN/partiel/sujet2024_2025.py
@@ -2,6 +2,8 @@ import numpy as np
 #                             I- Integration des EDO
 
 # Ex_1 : 
+def phi(t):
+    return 0
 
 def f(t, u, a):
     return np.array([u[1], u[2], a*np.sin(u[1] + u[0]**2)]) + phi(t)
